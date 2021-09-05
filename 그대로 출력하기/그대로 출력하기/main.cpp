eacdb211b15e819fedae8b956672ac11879541d1
KingRoh <53923162+KingRoh@users.noreply.github.com>
2021-09-05 11:58:06 +0900

    그대로 출력하기(11718)

---
+++ 그대로 출력하기/그대로 출력하기/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string a;
+	while (1)
+	{
+		getline(cin, a);
+		if (a == "")
+			break;
+		cout << a << endl;
+	}
+}
